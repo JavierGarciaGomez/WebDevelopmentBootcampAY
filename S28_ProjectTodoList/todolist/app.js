294cce4c1d8b16716551dd89d6612a0d88d6887e
JGG <javieron.garcia@gmail.com>
2021-01-07 22:49:48 +0100

    S28_TodoList

--- S28_ProjectTodoList/todolist/app.js
+++ S28_ProjectTodoList/todolist/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 // 342
 const mongoose = require("mongoose");
 const app = express();
+// 348 lodash
+const _ = require('lodash');
 // 266
 // 272 MODULES
 app.set('view engine', 'ejs'); // calling ejs
@@ -26,8 +28,16 @@ const itemSchema = new mongoose.Schema({
         required: [true, "Name is required"]
     }
 });
-
+// 346
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+// 342
 const Item = mongoose.model("Item", itemSchema);
+// 346
+const List = mongoose.model('List', listSchema);
+
 
 // 342 Insert items (I used the seeds/item.js)
 // 343 Rendering the items
@@ -42,37 +52,86 @@ app.get("/", (req, res) => {
     });
 });
 
-// 268, 271, 344
+// 268, 271, 344, 347
 app.post("/", (req, res) => {
     let itemName = req.body.newItem;
+    let listName = req.body.listName;
     let item = new Item({
         name: itemName
     });
-    item.save();
-    res.redirect("/");
+
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({
+            name: listName
+        }, (err, foundList) => {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
 })
 
-// 345
+// 345, 348
 app.post("/delete", (req, res) => {
-    Item.findByIdAndRemove(req.body.checkbox, (err) => {});
-    res.redirect("/");
-})
+    let checkedItemId = req.body.checkbox;
+    let listName = req.body.listName;
 
-// 346
-app.get("/:listName", (req, res) => {
-    let listName = req.params.listName;
-    const listSchema = {
-        name: String,
-        items: [itemsSchema]
+    if (listName === "Today") {
+        Item.findByIdAndRemove(req.body.checkbox, (err) => {});
+        res.redirect("/");
+    } else {
+        List.findOne({
+            name: listName
+        }, (err, foundList) => {
+            List.findOneAndUpdate({
+                name: listName
+            }, {
+                $pull: {
+                    items: {
+                        _id: checkedItemId
+                    }
+                }
+            }, (err, foundList) => {
+                if (!err) {
+                    res.redirect("/" + listName);
+                }
+            })
+        });
     }
 
-    const List = mongoose.model(List, listSchema);
-    const list = new List({
-        name: listName,
-        items: []
-    })
 
-    res.redirect("/");
+})
+
+// 346, 348
+app.get("/:listName", (req, res) => {
+    let listName = _.capitalize(req.params.listName);
+    List.findOne({
+        name: listName
+    }, (err, foundList) => {
+        if (!err) {
+            if (!foundList) {
+                console.log("not founded");
+                let item1 = new Item({
+                    name: 'Go to the meeting'
+                });
+                const list = new List({
+                    name: listName,
+                    items: [item1]
+                })
+                list.save();
+                res.redirect("/" + listName);
+            } else {
+                console.log("founded");
+                res.render('list', {
+                    listTitle: foundList.name,
+                    items: foundList.items
+                });
+            }
+        }
+    });
 })
 
 // 271
